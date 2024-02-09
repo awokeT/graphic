cde5ad34032fe147a17e1e50877f421a50f7494a
AwokeT <tiagoeberhardtflor1996@gmail.com>
2024-02-09 11:32:12 -0400

    chart updated and upgraded
    now its usefull and good to change.

--- src/components/Graphic.js
+++ src/components/Graphic.js
@@ -2,8 +2,14 @@ import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
 
 const createLineChart = (finalData, chartRef) => {
-    const { max } = finalData;
+    const { max, title, obs } = finalData;
+    let obsStr = 'OBS: '
+    const tensionValue = 0;
+    const borderWidthValue = 1.5;
     const ctx = chartRef.current.getContext('2d');
+    if(obs === '') {
+        obsStr = ''
+    }
 
     ctx.globalCompositeOperation = 'destination-over';
     ctx.fillStyle = 'transparent';
@@ -26,41 +32,54 @@ const createLineChart = (finalData, chartRef) => {
         labels: finalData.labels.slice(1),
         datasets: [
             {
-            label: 'Acertos',
-            data: finalData.dataset1,
-            fill: false,
-            borderColor: 'rgb(0,128,0)',
-            backgroundColor: 'rgb(0,128,0)',
-            tension: 0.1,
+                label: 'Acertos',
+                data: finalData.dataset1,
+                fill: false,
+                borderColor: 'rgb(0,128,0)',
+                backgroundColor: 'rgb(0,128,0)',
+                tension: tensionValue,
+                borderWidth: borderWidthValue
             },
             {
-            label: 'Erros',
-            data: finalData.dataset2,
-            fill: false,
-            borderColor: 'rgb(255,0,0)',
-            backgroundColor: 'rgb(255,0,0)',
-            tension: 0.1,
+                label: 'Erros',
+                data: finalData.dataset2,
+                fill: false,
+                borderColor: 'rgb(255,0,0)',
+                backgroundColor: 'rgb(255,0,0)',
+                tension: tensionValue,
+                borderWidth: borderWidthValue
             },
             {
-            label: 'Acertos com Suporte',
-            data: finalData.dataset3,
-            fill: false,
-            borderColor: 'rgb(255, 255, 0)',
-            backgroundColor: 'rgb(255, 255, 0)',
-            tension: 0.1,
+                label: 'Acertos com Suporte',
+                data: finalData.dataset3,
+                fill: false,
+                borderColor: 'rgb(0, 0, 255)',
+                backgroundColor: 'rgb(0, 0, 255)',
+                tension: tensionValue,
+                borderWidth: borderWidthValue
             },
             {
-            label: 'Demandas',
-            data: finalData.total,
-            fill: false,
-            borderColor: 'rgb(0, 0, 0)',
-            backgroundColor: 'rgb(0, 0, 0)',
-            tension: 0.1,
+                label: 'Aplicações',
+                data: finalData.total,
+                fill: false,
+                borderColor: 'rgb(0, 0, 0)',
+                backgroundColor: 'rgb(0, 0, 0)',
+                tension: tensionValue,
+                borderWidth: borderWidthValue
             },
         ],
         },
         options: {
             plugins: {
+                title: {
+                    display: true,
+                    text: title
+                },
+                subtitle: {
+                    display: true,
+                    text: obsStr + obs,
+                    position: 'bottom'
+                },
                 customCanvasBackgroundColor: {
                   color: 'white',
                 }
@@ -70,7 +89,7 @@ const createLineChart = (finalData, chartRef) => {
                 beginAtZero: true,
                 max: max
                 },
-        },
+            },
         },
         plugins: [plugin],
     });
@@ -78,12 +97,14 @@ const createLineChart = (finalData, chartRef) => {
     return myChart;
     };
 
-    const LineChart = ({ labels, dataset1, dataset2, dataset3, total, max }) => {
+    const LineChart = ({ labels, dataset1, dataset2, dataset3, total, max, title, obs, obsStr }) => {
     const chartRef = useRef(null);
 
     useEffect(() => {
         const myChart = createLineChart(
             {
+                title: title,
+                obs: obs,
                 labels: labels,
                 dataset1: dataset1,
                 dataset2: dataset2,
@@ -97,7 +118,7 @@ const createLineChart = (finalData, chartRef) => {
         return () => {
             myChart.destroy();
         };
-    }, [labels, dataset1, dataset2, dataset3, total, max, chartRef]);
+    }, [labels, dataset1, dataset2, dataset3, total, max, title, obs, chartRef]);
     
     return <canvas ref={chartRef} width={300} height={200} style={{ maxWidth: '100%' }} />;
     };
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,12 @@
 import React, { useState, useRef } from 'react';
 import Graphic from '../components/Graphic'
 
+
 export default function MainPage() {
-    const [data, setData] = useState({acertos: '', erros: '', acertosComSuporte: ''})
+    const [data, setData] = useState({acertos: '', erros: '', acertosComSuporte: '', title: '', obs: ''})
     const [finalData, setFinalData] = useState({
+        title: '',
+        obs: '',
         dataset1: [0],
         dataset2: [0],
         dataset3: [0],
@@ -38,6 +41,7 @@ export default function MainPage() {
     }
 
     function handleClick() {
+        const {title, obs} = data;
         const dataset1 = data.acertos.split(',');
         const dataset2 = data.erros.split(',');
         const dataset3 = data.acertosComSuporte.split(',');
@@ -48,8 +52,9 @@ export default function MainPage() {
 
         const total = somarArrays(dataset1,dataset2,dataset3)
         const max = maxNum(total);
-
         setFinalData({
+            title,
+            obs,
             dataset1,
             dataset2,
             dataset3,
@@ -57,18 +62,21 @@ export default function MainPage() {
             max,
             total,
         });
-        
     }
 
     return(
         <div className='main'>
             <div className="container">
-                <h2>Acertos</h2>
+                <h3>Nome do Grafico</h3>
+                <input value={data.title} name='title' className="input-text" type="text" onChange={ handleChange }/>
+                <h3>Acertos</h3>
                 <input value={data.acertos} name='acertos' className="input-text" type="text" onChange={ handleChange }/>
-                <h2>Erros</h2>
+                <h3>Erros</h3>
                 <input value={data.erros} name='erros' className="input-text" type="text" onChange={ handleChange }/>
-                <h2>Acertos com Suporte</h2>
+                <h3>Acertos com Suporte</h3>
                 <input value={data.acertosComSuporte} name='acertosComSuporte' className="input-text" type="text" onChange={ handleChange }/>
+                <span>Observações</span>
+                <textarea value={data.obs} name='obs' className="textarea" type="text" maxLength={240} onChange={ handleChange } />
                 <div className='div-button'>
                     <button className='button' onClick={handleClick}type='button'>Gerar Gráfico</button>
                 </div>
